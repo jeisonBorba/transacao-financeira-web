5443beefea843bcba71b042adeadfcf513843057
jeisonBorba <jeizon.borba@gmail.com>
2020-04-25 17:16:29 -0300

    transacao financeira web

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Container, Link } from './styles';
+
+import Logo from '../../assets/logo.svg';
+
+interface HeaderProps {
+  size?: 'small' | 'large';
+}
+
+const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
+  <Container size={size}>
+    <header>
+      <img src={Logo} alt="GoFinances" />
+      <nav>
+        <Link activeClassName="active" to="/" exact>
+          Listagem
+        </Link>
+        <Link activeClassName="active" to="/import">
+          Importar
+        </Link>
+      </nav>
+    </header>
+  </Container>
+);
+
+export default Header;
